f951070a897c6d2716e777c3952356c2000862e7
vijai-ks <vijai.ksv@gmail.com>
2023-09-18 09:08:47 +0530

    feat: quizzler-home: Add true button
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -14,29 +14,48 @@ class Quizzler extends StatefulWidget {
 class _QuizzlerState extends State<Quizzler> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Center(
-                    child: Text(
-                      'This is where the question text will go',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Expanded(
+                  flex: 5,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Center(
+                      child: Text(
+                        'This is where the question text will go',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              )
-            ],
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'True',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
