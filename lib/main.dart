eca7f4f19296b3dde41c89b21fd87b02774a47e9
vijai-ks <vijai.ksv@gmail.com>
2023-09-19 20:15:55 +0530

    feat: quizzler-home: Add alert pop-up for finish
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'quizz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() {
-  runApp(const Quizzler());
+  runApp(
+    const MaterialApp(home: Quizzler()),
+  );
 }
 
 QuizBrain quizzBrain = QuizBrain();
@@ -23,6 +26,22 @@ class _QuizzlerState extends State<Quizzler> {
     setState(() {
       if (quizzBrain.getTotalNumberOfQuestions() ==
           quizzBrain.getQuestionNumber()) {
+        Alert(
+          context: context,
+          type: AlertType.error,
+          title: "Quizzler ALERT",
+          desc: "You have reached the end of the Quiz.",
+          buttons: [
+            DialogButton(
+              onPressed: () => Navigator.pop(context),
+              width: 120,
+              child: const Text(
+                "Restart",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            )
+          ],
+        ).show();
         correctAnswers = 0;
         scoreKeeper.clear();
       }
@@ -48,160 +67,158 @@ class _QuizzlerState extends State<Quizzler> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey,
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: const Center(
-            child: Text('Quizzler'),
-          ),
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Center(
+          child: Text('Quizzler'),
         ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Center(
-                      child: Text(
-                        quizzBrain.getQuestionFromBank(),
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 24,
-                        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Center(
+                    child: Text(
+                      quizzBrain.getQuestionFromBank(),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 24,
                       ),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20.0, 10),
-                    child: TextButton(
-                      onPressed: () {
-                        checkAndUpdateAnswer(true);
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.green,
-                      ),
-                      child: const Text(
-                        'True',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20.0, 10),
+                  child: TextButton(
+                    onPressed: () {
+                      checkAndUpdateAnswer(true);
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
+                    child: const Text(
+                      'True',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
                       ),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20.0, 10),
-                    child: TextButton(
-                      onPressed: () {
-                        checkAndUpdateAnswer(true);
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.red,
-                      ),
-                      child: const Text(
-                        'False',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20.0, 10),
+                  child: TextButton(
+                    onPressed: () {
+                      checkAndUpdateAnswer(true);
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    child: const Text(
+                      'False',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
                       ),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    const Text(
+                      'Answer: ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      width: 300,
+                      height: 25,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Row(
+                          children: scoreKeeper,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       const Text(
-                        'Answer: ',
+                        'Score: ',
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 15,
+                          fontSize: 17,
                         ),
                       ),
-                      Container(
-                        color: Colors.white,
-                        width: 300,
-                        height: 25,
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Row(
-                            children: scoreKeeper,
-                          ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: Text('$correctAnswers'),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const Text(
+                        'Current Question : ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                      Text(
+                        quizzBrain.getQuestionNumber().toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                      const Text(
+                        '/',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                      Text(
+                        quizzBrain.getTotalNumberOfQuestions().toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
                         ),
                       ),
                     ],
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Score: ',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
-                          child: CircleAvatar(
-                            backgroundColor: Colors.white,
-                            child: Text('$correctAnswers'),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        const Text(
-                          'Current Question : ',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
-                          ),
-                        ),
-                        Text(
-                          quizzBrain.getQuestionNumber().toString(),
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
-                          ),
-                        ),
-                        const Text(
-                          '/',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
-                          ),
-                        ),
-                        Text(
-                          quizzBrain.getTotalNumberOfQuestions().toString(),
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
